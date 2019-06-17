1505e4769196115a369b28cb498cdc6ba94a0635
Rahmah-Nampala <rahma10na@gmail.com>
2019-06-17 08:57:17 +0300

    program run

---
+++ null_checker.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <=10; i++) // for a program that prompts for a new number 10 times
+{
+	let num = prompt ("Please enter number");
+	// The screen prompt should print
+	if (num<0)
+		console.log(-1);
+	else if (num<0)
+		console.log(1);
+	else
+		console.log(0);
+}
+	
